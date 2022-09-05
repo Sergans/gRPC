c0ee86210f881b0e122a40e9c6eb3d3371fa749f
Sergans <gan7er@yandex.ru>
2022-09-05 11:43:55 +0300

    add models

---
+++ ClinicService/Services/IAuthenticateService.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+
+namespace ClinicService.Services
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+
+        public SessionInfo GetSessionInfo(string sessionToken);
+    }
+}
